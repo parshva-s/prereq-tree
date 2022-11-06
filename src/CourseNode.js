4eeb555ca5e8862b1dda3b8d3193a7877498ca85
roljy <62124462+roljy@users.noreply.github.com>
2022-11-06 00:55:35 -0600

    CourseNode.objectify() works!

--- src/CourseNode.js
+++ src/CourseNode.js
@@ -55,37 +55,89 @@ export default class CourseNode {
      *  - this.prereqs and this.coreqs are lists of lists of strings
      */
     objectify() {
-        for (let i = 0; i < this.prereqs.length; i++) {
-            if (typeof(this.prereqs[i]) == "string") {
-                // as opposed to a subarray. can operate on the object directly
-                if (!CourseNode.nameToObjectPairs[this.prereqs[i]].objectified) {
-                    // the corresp. child is not objectified, and we must
-                    //  objectify it before adding it.
-                    CourseNode.nameToObjectPairs(this.prereqs[i]).objectify();
+        // blasted nested fors. is there a better way to do this?
+        // check that the pre/coreqs are not null before attempting to iterate through them
+        // objectify all the prereqs first.
+        if (this.prereqs !== null) {
+            for (let i = 0; i < this.prereqs.length; i++) {
+                if (typeof(this.prereqs[i]) == "string") {
+                    // as opposed to a subarray. can operate on the object directly
+                    if (!CourseNode.nameToObjectPairs[this.prereqs[i]].objectified) {
+                        // the corresp. child is not objectified, and we must
+                        //  objectify it before adding it.
+                        CourseNode.nameToObjectPairs[this.prereqs[i]].objectify();
+                    }
+                    // the corresp. child is now objectified, can replace
+                    //  our list's str directly with the child object and move on.
+                    this.prereqs[i] = CourseNode.nameToObjectPairs[this.prereqs[i]];
                 }
-                // the corresp. child is now objectified, can replace
-                //  our list's str directly with the child object and move on.
-                this.prereqs[i] = CourseNode.nameToObjectPairs[this.prereqs[i]];
-            }
-            else {
-                // subarray of children OR'd with each other.
-                //  repeat exactly what we've done but on the subarray elements
-                for (let j = 0; j < this.prereqs[i].length; j++) {
-                    if (typeof(this.prereqs[i][j]) == "string") {
-                        if (!CourseNode.nameToObjectPairs[this.prereqs[i][j]].objectified) {
-                            CourseNode.nameToObjectPairs(this.prereqs[i][j]).objectify();
+                else {
+                    // subarray of children OR'd with each other.
+                    //  repeat exactly what we've done but on the subarray elements
+                    for (let j = 0; j < this.prereqs[i].length; j++) {
+                        if (typeof(this.prereqs[i][j]) == "string") {
+                            if (!CourseNode.nameToObjectPairs[this.prereqs[i][j]].objectified) {
+                                CourseNode.nameToObjectPairs[this.prereqs[i][j]].objectify();
+                            }
+                            this.prereqs[i][j] = CourseNode.nameToObjectPairs[this.prereqs[i][j]];
                         }
-                        this.prereqs[i][j] = CourseNode.nameToObjectPairs[this.prereqs[i][j]];
-                    }
-                    else {
-                        // subsubarray of children AND'ed with each other
-                        // TODO: finish objectify
+                        else {
+                            // subsubarray of children AND'ed with each other.
+                            // repeat once more
+                            for (let k = 0; k < this.prereqs[i][j].length; k++) {
+                                // assume all elements are strings from now on.
+                                if (!CourseNode.nameToObjectPairs[this.prereqs[i][j][k]].objectified) {
+                                    CourseNode.nameToObjectPairs[this.prereqs[i][j][k]].objectify();
+                                }
+                                this.prereqs[i][j][k] = CourseNode.nameToObjectPairs[this.prereqs[i][j][k]];
+                            }
+                        }
                     }
                 }
-
             }
         }
 
+        // now objectify all the coreqs after.
+        if (this.coreqs !== null) {
+            for (let i = 0; i < this.coreqs.length; i++) {
+                if (typeof(this.coreqs[i]) == "string") {
+                    // as opposed to a subarray. can operate on the object directly
+                    if (!CourseNode.nameToObjectPairs[this.coreqs[i]].objectified) {
+                        // the corresp. sibling is not objectified, and we must
+                        //  objectify it before adding it.
+                        CourseNode.nameToObjectPairs[this.coreqs[i]].objectify();
+                    }
+                    // the corresp. sibling is now objectified, can replace
+                    //  our list's str directly with the sibling object and move on.
+                    this.coreqs[i] = CourseNode.nameToObjectPairs[this.coreqs[i]];
+                }
+                else {
+                    // subarray of siblings OR'd with each other.
+                    //  repeat exactly what we've done but on the subarray elements
+                    for (let j = 0; j < this.coreqs[i].length; j++) {
+                        if (typeof(this.coreqs[i][j]) == "string") {
+                            if (!CourseNode.nameToObjectPairs[this.coreqs[i][j]].objectified) {
+                                CourseNode.nameToObjectPairs[this.coreqs[i][j]].objectify();
+                            }
+                            this.coreqs[i][j] = CourseNode.nameToObjectPairs[this.coreqs[i][j]];
+                        }
+                        else {
+                            // subsubarray of siblings AND'ed with each other.
+                            // repeat once more
+                            for (let k = 0; k < this.coreqs[i][j].length; k++) {
+                                // assume all elements are strings from now on.
+                                if (!CourseNode.nameToObjectPairs[this.coreqs[i][j][k]].objectified) {
+                                    CourseNode.nameToObjectPairs[this.coreqs[i][j][k]].objectify();
+                                }
+                                this.coreqs[i][j][k] = CourseNode.nameToObjectPairs[this.coreqs[i][j][k]];
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // after objectifying *all* children+siblings, this too is now objectified
         this.#objectified = true;
     }
 
