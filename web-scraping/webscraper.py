692106821bbc9e460c6025178f0d48215577bb95
parshva-s <63024362+parshva-s@users.noreply.github.com>
2022-11-05 18:05:00 -0600

    Webscraping ad csv file complete

--- web-scraping/webscraper.py
+++ web-scraping/webscraper.py
@@ -2,50 +2,64 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 
-
+# https://www.scrapingbee.com/blog/selenium-python/
 options = Options()
 options.headless = True
 options.add_argument("--window-size=1920,1200")
-text = {}
 
+#driver needed in Downloads (https://chromedriver.chromium.org/downloads)
 DRIVER_PATH = '~/Downloads'
 driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
 driver.get('https://apps.ualberta.ca/catalogue/course/ece')
-allCourses = driver.find_elements(By.XPATH, "//div[@class='card']//h2")
-# allCourses.remove(allCourses[0])
-# allCoursesDetails = driver.find_elements(By.TAG_NAME, "p")
-allCoursesDetails = driver.find_elements(By.XPATH,"//div[@class='card']//p")
-# print(len(allCoursesDetails))
-# print(len(allCourses))
+allCourses = driver.find_elements(By.XPATH, "//div[@class='card']//h2")  # course title
+allCoursesDetails = driver.find_elements(By.XPATH,"//div[@class='card']//p")  # course details
+
+courses = {}  # saves all course info
+
 for i in range(len(allCoursesDetails)):
     addPre = False
     addCor = False
     prereq = ""
     coreq = ""
-    # print(allCoursesDetails[i].text)
     courseDetails = allCoursesDetails[i].text.split()
+
+    # find prereq and coreqs
     for j in courseDetails:
         # for prereqs
         if j.lower() == "prerequisites:" or j.lower()=="prerequisite:":
             addPre = True
+            continue
         elif addPre and j[(len(j))-1] == ".":
             prereq += j.strip(".")
             addPre = False
+        
         # for coreqs
         if j.lower() == "corequisites:" or j.lower() == "corequisite:":
             addCor = True
+            continue
         elif addCor and j[(len(j))-1] == ".":
             coreq += j.strip(".")
             addCor = False
+        
+        #add prereqs/coreqs present
         if addPre:
             prereq += j + " "
         if addCor:
             coreq += j + " "
+
     if prereq == "": prereq = None
     if coreq == "": coreq = None
-    text[allCourses[i].text] = [prereq, coreq]
-for key, value in text.items():
+    courses[allCourses[i].text] = [prereq, coreq]
+
+
+# create csv file to write data to
+fileName = "web-scraping/webscrapingdata.csv"
+f = open(fileName,"w")
+f.write("Name,Title,Prereqs,Coreqs\n")
+
+for key, value in courses.items():
     key = key.split(" - ")
-    print("Course Name: " + key[0] +  " Course Title: " + key[1] + " -- Prereq: " + str(value[0]) + " and Coreq: " + str(value[1]))
-# print(text)
+    f.write(key[0] + ",\"" + key[1] + "\",\"" + str(value[0]) + "\",\"" + str(value[1]) + "\"\n")
+
+f.close()
 driver.quit()
